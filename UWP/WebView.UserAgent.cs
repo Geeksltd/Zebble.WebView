e2d6804f2573428cb09a67cc4ba2acec13504b51
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2023-03-06 14:15:38 +0330

    Update UAP's user agents

--- UWP/WebView.UserAgent.cs
+++ UWP/WebView.UserAgent.cs
@@ -12,13 +12,13 @@
 
         static void SetDefaultUserAgent()
         {
-            var userAgent = "Mozilla/5.0 (compatible; MSIE 10.0; WebView/3.0; Windows Phone 10.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 925)";
+            var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4501.0 Safari/537.36 Edg/91.0.866.0";
 
             if (Services.CssEngine.Platform == DevicePlatform.IOS)
-                userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25";
+                userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/604.1";
 
             if (Services.CssEngine.Platform == DevicePlatform.Android)
-                userAgent = "Mozilla/5.0 (Linux; Android 4.4.2; en-us; SAMSUNG SM-G900T Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Version/1.6 Chrome/28.0.1500.94 Mobile Safari/537.36";
+                userAgent = "Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.153 Mobile Safari/537.36";
 
             UrlMkSetSessionOption(URLMON_OPTION_USERAGENT, userAgent, userAgent.Length, 0);
         }
